960740e72ab69c058a6c4395e40ce4fc207b4aa1
Martin Wheatley <mwhm@jyskerealkredit.dk>
2022-07-15 08:06:18 +0200

    Initial commit

---
+++ src/Chapter1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace category_theory;
+internal static class Chapter1
+{
+    public static T Id<T>(T x) => x;
+
+    public static Func<A, C> Compose<A, B, C>(Func<A, B> f, Func<B, C> g) => a => g(f(a));
+}
---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace category_theory;
+
+public class Program
+{
+    public static int Main()
+    {
+        var d = 5.3m;
+        var id = Chapter1.Id(d);
+
+        var comp = Chapter1.Compose<decimal, decimal, decimal>(
+            dec => Chapter1.Id(dec),
+            dec => Chapter1.Id(dec));
+
+        var result = comp(d);
+
+        Console.WriteLine(id.Equals(d));
+
+        return 0;
+    }
+}
