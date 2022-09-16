26f8ee409690c2ed71a0d61ca58c7a88bd54f9e5
Martin Wheatley <mwhm@jyskerealkredit.dk>
2022-09-16 12:59:57 +0200

    Added NonEmptyReadOnlyList and Value/Range experiments...

---
+++ src/Chapter2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace category_theory;
+
+internal static class Chapter2
+{
+
+}
+
+internal class Memoize<T1, T2>
+    where T1 : IEquatable<T1>
+{
+    private readonly Func<T1, T2> _fallbackFunc;
+    private readonly IReadOnlyDictionary<T1, T2> _memory;
+
+    public Memoize(Func<T1, T2> fallbackFunc)
+    {
+        _fallbackFunc = fallbackFunc;
+        _memory = new Dictionary<T1, T2>();
+    }
+
+    public T2 Evaluate(T1 t) =>
+        _memory.ContainsKey(t)
+            ? _memory[t]
+            : _fallbackFunc(t);
+}
+
+internal sealed class NonEmptyReadOnlyList<T> : IReadOnlyList<T>
+{
+    private readonly IReadOnlyList<T> _values;
+
+    public NonEmptyReadOnlyList(T value)
+        : this(new T[1] { value })
+    {
+    }
+
+    public NonEmptyReadOnlyList(IReadOnlyList<T> values)
+    {
+        if (values is null || values.Count is 0 || values[0] is null)
+            throw new Exception("");
+
+        _values = values.ToArray();
+        First = _values[0];
+    }
+
+    public T First { get; }
+    public T this[int index] => _values[index];
+    public T this[Index index] => _values[index];
+    public int Count => _values.Count;
+
+    public IEnumerator<T> GetEnumerator() => _values.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _values.GetEnumerator();
+}
+
+internal readonly struct Value<T>
+    where T : struct, IMinValue<T>, IMaxValue<T>
+{
+    private readonly T _value;
+
+    public Value(T? value)
+    {
+        _value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public static implicit operator Value<T>(T value) => new(value);
+}
+
+internal static class Value
+{
+    public static Value<T> Create<T>(T value)
+        where T : struct, IMinValue<T>, IMaxValue<T> => new(value);
+}
+
+internal interface IMinValue<T>
+{
+    T MinValue { get; }
+}
+
+internal interface IMaxValue<T>
+{
+    T MaxValue { get; }
+}
+
+internal interface IValueIncrementor<T> where T : IMinValue<T>, IMaxValue<T>
+{
+    T GetNext(T value);
+}
+
+internal sealed class Range<T>
+    where T : struct, IMinValue<T>, IMaxValue<T>
+{
+    private readonly T _start;
+    private readonly T _end;
+    private readonly IValueIncrementor<T> _valueIncrementor;
+
+    public Range(T start, T end, IValueIncrementor<T> valueIncrementor)
+    {
+        _start = start;
+        _end = end;
+        _valueIncrementor = valueIncrementor;
+    }
+}
+
+public readonly struct DateOnlyValue : IMinValue<DateOnly>, IMaxValue<DateOnly>
+{
+    private readonly DateOnly _value;
+
+    public DateOnlyValue(DateOnly value)
+    {
+        _value = value;
+    }
+
+    public DateOnly MinValue => DateOnly.MinValue;
+    public DateOnly MaxValue => DateOnly.MaxValue;
+
+    public static implicit operator DateOnlyValue(DateOnly value) => new(value);
+}
--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,24 @@ public class Program
 {
     public static int Main()
     {
+        int[] invalidArr1 = null!;
+        var invalidArr2 = Array.Empty<int>();
+        var validArr1 = new int[1] { 1 };
+        var validArr2 = Enumerable.Range(0, 10).ToList();
+
+        var nonEmptyValid1 = new NonEmptyReadOnlyList<int>(1); // should NOT throw
+        var nonEmptyValid2 = new NonEmptyReadOnlyList<int>(validArr1); // should NOT throw
+        var nonEmptyValid3 = new NonEmptyReadOnlyList<int>(validArr2); // should NOT throw
+
+        var nonEmptyInvalid1 = new NonEmptyReadOnlyList<int?>((int?)null); // should throw
+        var nonEmptyInvalid2 = new NonEmptyReadOnlyList<int>(invalidArr1); // should throw
+        var nonEmptyInvalid3 = new NonEmptyReadOnlyList<int>(invalidArr2); // should throw
+
+        foreach (var item in nonEmptyValid1)
+        {
+            _ = item.Equals(nonEmptyValid1.First);
+        }
+
         var d = 5.3m;
         var id = Chapter1.Id(d);
 
@@ -13,8 +31,19 @@ public class Program
 
         var result = comp(d);
 
+        var mem = new Memoize<int, DateOnly>(i => DateOnly.FromDayNumber(i));
+
+        _ = mem.Evaluate(5);
+
         Console.WriteLine(id.Equals(d));
 
         return 0;
+    }
+
+    public static void Main2()
+    {
+        var val = new Value<DateOnlyValue>(new DateOnlyValue(new DateOnly(2022, 1, 1)));
+
+        var start = Value.Create<DateOnlyValue>(new DateOnly(2022, 1, 1)));
     }
 }
