cdf8b26de98482d89bbf077cb460d1576e3c730d
Martin Wheatley <martinwheatley92@gmail.com>
2022-10-01 20:10:14 +0200

    Opdateret build-project

--- build/Build.cs
+++ build/Build.cs
@@ -5,6 +5,8 @@ using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
@@ -13,32 +15,75 @@ using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution]
+    private readonly Solution Solution;
+
+    [GitVersion]
+    private readonly GitVersion GitVersion;
+
+    public AbsolutePath ArtifactsDir => RootDirectory / "artifacts";
+
+    private const string _author = "Martin Wheatley";
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetTasks.DotNetRestore(settings => settings
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetBuild(settings => settings
+                .SetNoRestore(true)
+                .SetAuthors(_author)
+                .SetPackageId("Wheatley.Prelude")
+                .SetTreatWarningsAsErrors(true)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetAssemblyVersion(GitVersion.MajorMinorPatch)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetProperty("Commit", GitVersion.ShortSha)
+                .SetConfiguration(Configuration)
+                .SetProjectFile(Solution));
         });
 
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(settings => settings
+                .SetProject(Solution.GetProject("category-theory"))
+                .SetConfiguration(Configuration)
+                .SetNoBuild(true)
+                .SetNoRestore(true)
+                .SetOutputDirectory(ArtifactsDir));
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(settings => settings
+                .SetNoBuild(true)
+                .SetNoRestore(true)
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration));
+            ;
+            // DotNetTasks.DotNetTest(settings =>
+            //     settings.SetProject)
+        });
 }
